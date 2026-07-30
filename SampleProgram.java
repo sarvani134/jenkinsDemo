f0feb6088bfd899d5356f45ba15b98ed5e0e9963
Sarvani Modumudi <modumudisarvani@gmail.com>
2026-07-30 11:07:21 +0530

    added and deleted

---
+++ SampleProgram.java
@@ -0,0 +1,16 @@
+public class SampleProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+        System.out.println("Welcome to Java Programming.");
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+        int diff = a - b;
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Difference = " + diff);
+    }
+}
--- Simple.java
+++
@@ -1,7 +0,0 @@
-
-public class Simple {
-   public static void main(String[] var0) {
-      System.out.println("Hello from Jenkins!");
-      System.out.println("Hello from Jenkins!");
-   }
-}
